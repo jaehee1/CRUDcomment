4f76a4933e07d3d4aed6df9614e11f2550da1180
xlwindlx <854575@naver.com>
2017-05-07 13:14:19 +0000

    validation

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,3 +1,5 @@
 class Comment < ActiveRecord::Base
     belongs_to :post
+    validates :content, absence: true
+    # 댓글 내용이 빈칸이면 안됨
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,5 @@
 class Post < ActiveRecord::Base
     has_many :comments
+    validates :title, :content, absence: true
+    # 글 제목, 내용이 빈칸이면 안됨
 end
